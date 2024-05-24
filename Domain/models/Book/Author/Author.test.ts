e0582b756a309d8d90c10364777da01502f6c157
jinchangz <jinchangz1@gmail.com>
2024-05-25 01:19:47 +0900

    Add Author.ts and test

---
+++ Domain/models/Book/Author/Author.test.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect } from "@jest/globals";
+import { Author } from "./Author";
+
+describe('Author', () => {
+  // 正常系
+  it('1文字以上５０文字以内は正常', () => {
+    const name = '夏目漱石';
+    expect(() => new Author(name)).not.toThrow();
+  });
+
+  // 異常系
+  it('空文字の場合はエラーを投げる', () => {
+    const name = '';
+    expect(() => new Author(name)).toThrow(`著者名は${Author.MIN_LENGTH}文字以上、${Author.MAX_LENGTH}文字以下でなければなりません。`);
+  });
+
+  it('５１文字以上の場合はエラーを投げる', () => {
+    const name = 'あ'.repeat(51)
+    expect(() => new Author(name)).toThrow(`著者名は${Author.MIN_LENGTH}文字以上、${Author.MAX_LENGTH}文字以下でなければなりません。`);
+  });
+});
