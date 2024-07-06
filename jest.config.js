78201fd8aa16f40d50cab30d3a94c26f85dec151
jinchangz <jinchangz1@gmail.com>
2024-07-06 11:07:21 +0900

    book.test.ts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,24 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+const fs = require('fs');
+const path = require('path');
+const { pathsToModuleNameMapper } = require('ts-jest');
+// tsconfig.jsonを読み込む
+const tsconfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'tsconfig.json'), 'utf8'));
+const { compilerOptions } = tsconfig;
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'app'],
   transformIgnorePatterns: ['/node_modules'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>',
+  }),
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
 };
